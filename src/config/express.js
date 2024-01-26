36301346d209a7cb3cdc938b0733db522db94509
JudisP <judisson429@hotmail.com>
2024-01-26 17:10:18 -0300

    Feat - Usando o cors

--- src/config/express.js
+++ src/config/express.js
@@ -1,8 +1,10 @@
+const cors = require('cors');
 const express = require('express')
 const app = express()
 const routes = require('../routes/route.js')
 
 app.use(express.json())
+app.use(cors());
 
 app.use('/', routes)
 
