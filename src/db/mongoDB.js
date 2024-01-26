bc2192a9e6b5c790b45352833d01c4fae5d4f64e
JudisP <judisson429@hotmail.com>
2024-01-26 16:24:31 -0300

    StyCode - Aspas

--- src/db/mongoDB.js
+++ src/db/mongoDB.js
@@ -1,18 +1,48 @@
-const mongoose = require("mongoose");
-const configDB = require("./configDB");
+const mongoose = require('mongoose');
+const configDB = require('./configDB');
 // const logger = require("pino")();
 // console.log('cofing.moongose', configDB.moongoose.enabled)
-
 
 const connectMongoDB = async () => {
   if (configDB.moongoose.enabled) {
     await mongoose
       .connect(configDB.moongoose.url)
       .then(() => {
-        console.log("Connected to MongoDB");
+        console.log('Connected to MongoDB');
       })
-      .catch((err) => console.error("Error connecting to MongoDB: ", err));
+      .catch((err) => {
+        console.error('Error connecting to MongoDB: ', err);
+        mongoose.connection.close();
+      });
   }
 };
 
-module.exports = connectMongoDB
+// mongoose.connection.on('connected', () => {
+//   console.log('Mongoose connected to DB.');
+// });
+
+// mongoose.connection.on('error', err => {
+//   console.error(`Mongoose connection error: ${err}`);
+// });
+
+// mongoose.connection.on('disconnected', () => {
+//   console.log('Mongoose disconnected.');
+// });
+
+// const gracefulExit = () => {
+//   mongoose.connection.close(() => {
+//     console.log('Mongoose connection closed through app termination.');
+//     // process.exit(0);
+//   });
+// };
+
+// // // Para encerramento da aplicação e nodemon restarts
+// // process.on('SIGINT', gracefulExit).on('SIGTERM', gracefulExit);
+
+// // Captura exceções não tratadas
+// process.on('uncaughtException', err => {
+//   console.error('Uncaught Exception:', err);
+//   gracefulExit();
+// });
+
+module.exports = connectMongoDB;
