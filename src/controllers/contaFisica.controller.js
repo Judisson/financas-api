abbe7551230b34051dd00248c77034253752b140
JudisP <judisson429@hotmail.com>
2024-01-26 17:10:42 -0300

    Refactor - Retirando a tipagem de data

--- src/controllers/contaFisica.controller.js
+++ src/controllers/contaFisica.controller.js
@@ -4,33 +4,33 @@ exports.Create = async (req, res) => {
   const data = await createTransaction(
     req.body || []
   )
-  return res.status(200).json({ data: data })
+  return res.status(200).json(data)
 }
 
 exports.Search = async (req, res) => {
   const data = await searchTransaction(
     req.body || []
   )
-  return res.status(200).json({ data: data })
+  return res.status(200).json(data)
 }
 
 exports.SearchList = async (req, res) => {
   const data = await searchTransactions(
     req.body || []
   )
-  return res.status(200).json({ data: data })
+  return res.status(200).json(data)
 }
 
 exports.Update = async (req, res) => {
   const data = await updateTransaction(
     req.body || []
   )
-  return res.status(200).json({ data: data })
+  return res.status(200).json(data)
 }
 
 exports.Delete = async (req, res) => {
   const data = await deleteTransaction(
     req.body || []
   )
-  return res.status(200).json({ data: data })
+  return res.status(200).json(data)
 }
