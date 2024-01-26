ca4d51b58ea563e14aa733eb2e9daca7b9d77f94
JudisP <judisson429@hotmail.com>
2024-01-26 16:47:36 -0300

    Feat - Criando a rota de cada funcionalidade do crud

---
+++ src/routes/contaFisica.route.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const controller = require('../controllers/alimentacao.controller');
+const router = express.Router();
+
+router.post('/transaction/create', controller.Create)
+router.get('/transaction/list', controller.SearchList)
+router.get('/transaction/search', controller.Search)
+router.put('/transaction/update', controller.Update)
+router.delete('/transaction/delete', controller.Delete)
+
+module.exports = router
--- src/routes/transaction.route.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const controller = require('../controllers/transaction.controller');
-const router = express.Router();
-
-router.post('/transactions', controller.Receive)
-
-module.exports = router
