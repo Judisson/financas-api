a54c7c6d91cda90d69ea3cf1af64dad1539236d7
JudisP <judisson429@hotmail.com>
2024-02-23 16:39:12 -0300

    Feat(cartaoCredito) - Criando o Controller

---
+++ src/controllers/cartaoCredito.controller.js
@@ -0,0 +1,43 @@
+const { createTransaction, searchTransaction, searchTransactions, updateTransaction, deleteTransaction, resumoTransactions } = require('../service/cartaoCreditoService')
+
+exports.Create = async (req, res) => {
+  const data = await createTransaction(
+    req.body || []
+  )
+  return res.status(200).json(data)
+}
+
+exports.Search = async (req, res) => {
+  const data = await searchTransaction(
+    req.query || []
+  )
+  return res.status(200).json(data)
+}
+
+exports.SearchList = async (req, res) => {
+  const data = await searchTransactions(
+    req.body || []
+  )
+  return res.status(200).json(data)
+}
+
+exports.Update = async (req, res) => {
+  const data = await updateTransaction(
+    req.body || []
+  )
+  return res.status(200).json(data)
+}
+
+exports.Delete = async (req, res) => {
+  const data = await deleteTransaction(
+    req.body || []
+  )
+  return res.status(200).json(data)
+}
+
+exports.Resumo = async (req, res) => {
+  const data = await resumoTransactions(
+    req.body || []
+  )
+  return res.status(200).json(data)
+}
