9abcbeaf61f9fb41b9c78fea5821af57c2fa4bc9
JudisP <judisson429@hotmail.com>
2024-02-23 16:40:43 -0300

    Feat(cartãoCredito) - Criando as Rotas

---
+++ src/routes/cartaoCredito.route.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const controller = require('../controllers/cartaoCredito.controller');
+const router = express.Router();
+
+router.post('/transaction/create', controller.Create)
+router.get('/transaction/list', controller.SearchList)
+router.get('/transaction/search', controller.Search)
+router.put('/transaction/update', controller.Update)
+router.delete('/transaction/delete', controller.Delete)
+router.get('/transactions/resumo', controller.Resumo)
+
+module.exports = router
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const AlimentacaoRoutes = require('./alimentacao.route')
+const CartaoCreditoRoutes = require('./cartaoCredito.route')
 const ContaFisicaRoutes = require('./contaFisica.route')
 const ContaPoupancaRoutes = require('./contaPoupanca.route')                                               
 
 router.use('/alimentacao', AlimentacaoRoutes);
+router.use('/cartao-credito', CartaoCreditoRoutes);
 router.use('/conta-fisica', ContaFisicaRoutes);
 router.use('/conta-poupanca', ContaPoupancaRoutes);
 
